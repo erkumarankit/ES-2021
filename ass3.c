44376f3b898ac1a6b8553ccf146b75e983d234ed
Sahil Saha <sahilsaha7773@gmail.com>
2021-06-08 21:00:38 +0530

    Init

---
+++ ass3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int x;
+	printf("Enter the number: ");
+	scanf("%d", &x);
+	int sum = 0;
+	int temp = x;
+	if(temp < 0){
+		temp*=(-1);
+	}
+	while(temp>0){
+		sum+=(temp%10);
+		temp/=10;
+	}
+	printf("Sum of all the digits of %d: %d", x, sum);
+}
